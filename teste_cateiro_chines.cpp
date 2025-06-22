f86cf6de46c6404cd9be70c47676f03586eecce3
victor-kauan-coder <victor.kauan@ufpi.edu.br>
2025-06-22 13:12:31 -0300

    coloquei uns print para ficar mais facil a vizualização no teste

--- teste_cateiro_chines.cpp
+++ teste_cateiro_chines.cpp
@@ -10,21 +10,24 @@ using namespace std;
 
 
 int main(){
-    int n,m; 
-    cin >> n >> m;
+    int qtdVertices,qtdArestas; 
+    cout << "Informe a quantidade de vertices e a de Arestas" <<endl;
+    cin >> qtdVertices >> qtdArestas;
     Grafo grafo;
-    for(int i=0;i <n;i++){
+    for(int i=0;i <qtdVertices;i++){
         Vertice* a = new Vertice();
         a -> id = i;
         grafo.adicionar_vertice(a);
     }
-
-    for(int i = 0;i<m;i++){
+    cout << "Insira: (vertice inicial vertice final peso) o primeiro vertice comeca no 0" << endl;
+    for(int i = 0;i<qtdArestas;i++){
         ll inicio,destino,peso; 
         cin >> inicio >> destino >> peso;
         grafo.vertices[inicio]->adicionar_aresta(peso,grafo.vertices[destino]);
         grafo.vertices[destino]->adicionar_aresta(peso,grafo.vertices[inicio]);
     }
+    cout << "Grafo original"<< endl;
+    grafo.imprimir_grafo(); 
     vector<Vertice*> vertices_impares = impares(&grafo);
     vector<vector<pair<Vertice*, Vertice*>>> resultado;
     emparelhamentos(&vertices_impares,&resultado);
@@ -46,7 +49,7 @@ int main(){
     for(auto  [vertice_inicial,aresta] : dup){
         duplicar_arestas(vertice_inicial, aresta);
     }
-
+    cout << "Grafo final"<< endl;
     grafo.imprimir_grafo();
 }
 //grafo que eu mandei no grupo 
